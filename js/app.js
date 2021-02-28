63de013254403436cde7bf98895433f4ab9d4887
Ahmed adam <67753514+Ahmed-SD@users.noreply.github.com>
2021-02-28 13:37:41 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const container = document.querySelector(".container")
+const coffees = [
+  { name: "Perspiciatis", image: "images/1.jpg" },
+ ,
+]
+const showCoffees = () => {
+    let output = ""
+    coffees.forEach(
+      ({ name, image }) =>
+        (output += `
+                <div class="card">
+                  <img class="card--avatar" src=${image} />
+                  <h1 class="card--title">${name}</h1>
+                  <a class="card--link" href="#">Taste</a>
+                </div>
+                `)
+    )
+    container.innerHTML = output
+  }
+  
+  document.addEventListener("DOMContentLoaded", showCoffees)
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceworker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
